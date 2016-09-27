530b1531fab01989370060775b3033e06576aa22
Raj Negi <raj.negi@weboniselab.com>
2016-09-27 18:02:13 +0530

    First version

---
+++ weboniselogin.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -X POST -H "Cache-Control: no-cache" -H "Postman-Token: 6a63004f-5575-e67a-42de-8303876c65fb" -d  'mode=191&username=rajn&password=rajnegi123&a=1474975622707&producttype=0' "http://10.0.0.1:8090/login.xml"
