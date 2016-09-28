991ccc233820ba4d26d5a43ed08e6b8446439388
Raj Negi <raj.negi@weboniselab.com>
2016-09-28 10:41:41 +0530

    Display better messages

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@ echo "Enter username: "
 read user_name
 echo "Enter password: "
 read user_password
-
-
+echo "Generating curl command..."
 printf "#!/bin/sh\ncurl -X POST -H \"Cache-Control: no-cache\" -H \"Postman-Token: 6a63004f-5575-e67a-42de-8303876c65fb\" -d  \"mode=191&username=$user_name&password=$user_password&a=1474975622707&producttype=0\" \"http://10.0.0.1:8090/login.xml\" " > weboniselogin.sh
-
+echo "Making a new cron job..."
 var=$(pwd)
 line="*/1 * * * * $var/weboniselogin.sh"
 (crontab -l; echo "$line" ) | crontab -
+echo "Done!"
